ba177927450340c7e84a28488a2d689d665fb935
Simon Schuster <simschu@gmx.de>
2022-11-10 15:25:47 +0100

    first layout without css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React and Morty App</h1>
+      </header>
+      <article>
+        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
+        <h2>Morty Smith</h2>
+      </article>
+      <footer>
+        <a href="#">Placeholder</a>
+        <a href="#">Placeholder</a>
+        <a href="#">Placeholder</a>
+        <a href="#">Placeholder</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
